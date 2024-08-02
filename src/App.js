b607804b15b3deebbb42b59fb6a9073f3cf1dd8e
jiwon <asdfoqlk@naver.com>
2024-08-02 11:38:56 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
+import Sub from './Sub';
 
 function App() {
+  // let number = 1; // 상태 값 아님
+  const [number, setNumber] = useState(2); // React 안에 hooks 라이브러리 상태값이 됨.
+  
+  const add = () => {
+    // number ++; 을 할 수 없다.
+     setNumber(number + 1); // 리액트한테 number 값 변경할께라고 요청 
+     console.log('add', number);
+  };
+  // 랜더링 시점 = 상태 값 변경
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <div>
+      <h1>숫자:{number}</h1>
+      <button onClick={add}>더하기</button>
     </div>
+    <Sub/>
+  </div>
   );
 }
 
---
+++ src/Sub.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Sub extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Sub;
